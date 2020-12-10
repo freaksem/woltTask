bb3b327189cec128b35f1a9ae8cca08a34cc1f33
freaksem@gmail.com <Password100500>
2020-12-10 07:29:21 +0300

    Move test data to json file

--- src/test/kotlin/com/semyon/wolt/WoltApplicationTests.kt
+++ src/test/kotlin/com/semyon/wolt/WoltApplicationTests.kt
@@ -22,86 +22,21 @@ class WoltApplicationTests {
     fun `Send JSON and assert response`() {
         val request = MockMvcRequestBuilders.post("/api/schedule")
             .contentType(MediaType.APPLICATION_JSON)
-            .content(
-                """
-				{
-				  "monday" : [],
-				  "tuesday" : [
-				    {
-				      "type" : "open",
-				      "value" : 32400
-				    },
-				    {
-				      "type" : "close",
-				      "value" : 39600
-				    },
-				    {
-				      "type" : "open",
-				      "value" : 57600
-				    },
-				    {
-				      "type" : "close",
-				      "value" : 82800
-				    }
-				  ],
-				  "wednesday" : [],
-				  "thursday" : [
-				    {
-				      "type" : "open",
-				      "value" : 36000
-				    },
-				    {
-				      "type" : "close",
-				      "value" : 64800
-				    }
-				  ],
-				  "friday" : [
-				    {
-				      "type" : "open",
-				      "value" : 36000
-				    }
-				  ],
-				  "saturday" : [
-				    {
-				      "type" : "close",
-				      "value" : 3600
-				    },
-				    {
-				      "type" : "open",
-				      "value" : 36000
-				    }
-				  ],
-				  "sunday" : [
-				    {
-				      "type" : "close",
-				      "value" : 3600
-				    },
-				    {
-				      "type" : "open",
-				      "value" : 43200
-				    },
-				    {
-				      "type" : "close",
-				      "value" : 75600
-				    }
-				  ]
-				}
-			""".trimIndent()
-            )
+            .content(javaClass.classLoader.getResource("data.json").readText())
 
         mockMvc.perform(request)
             .andExpect(status().isOk)
             .andExpect(
                 MockMvcResultMatchers.content().string(
                     """
-                Monday: Closed
-                Tuesday: 9:00 AM - 11:00 AM, 4:00 PM - 11:00 PM
-                Wednesday: Closed
-                Thursday: 10:00 AM - 6:00 PM
-                Friday: 10:00 AM - 1:00 AM
-                Saturday: 10:00 AM - 1:00 AM
-                Sunday: 12:00 PM - 9:00 PM
-                """.trimIndent()
+                    Monday: Closed
+                    Tuesday: 9:00 AM - 11:00 AM, 4:00 PM - 11:00 PM
+                    Wednesday: Closed
+                    Thursday: 10:00 AM - 6:00 PM
+                    Friday: 10:00 AM - 1:00 AM
+                    Saturday: 10:00 AM - 1:00 AM
+                    Sunday: 12:00 PM - 9:00 PM
+                    """.trimIndent()
                 )
             )
     }
